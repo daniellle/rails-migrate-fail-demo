f171f3b2cb2c2727963f22515f7be86eb6fd2787
Danielle <p13pinheiro@gmail.com>
2025-07-28 20:03:41 -0300

    Update constants.rb

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,7 @@
-# This initializer will blow up if the ENV var is missing
-VALUE = ENV.fetch("SIDEKIQ_MEDIA_STREAMS_REDIS_URL")
-puts "Got URL: #{VALUE}"
+begin
+  VALUE = ENV.fetch("SIDEKIQ_MEDIA_STREAMS_REDIS_URL")
+  puts "Got URL: #{VALUE}"
+rescue KeyError => e
+  puts "[WARNING] Missing ENV var SIDEKIQ_MEDIA_STREAMS_REDIS_URL: #{e.message}"
+  VALUE = nil
+end
